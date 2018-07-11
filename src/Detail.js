ba3c617dae420e018a8dad7fee30fdaf7c606d51
sunshine2329 <seniordeveloper00001@gmail.com>
2018-07-11 01:27:30 +0100

    Details Route with params

--- src/Detail.js
+++ src/Detail.js
@@ -1,5 +1,9 @@
 import React from "react";
+// import { Query } from "react-apollo";
 
-const Detail = () => <div>Detail</div>;
+const Detail = ({ match: { params: { movieId }}}) => {
+  console.log(movieId);
+  return <div>Detail</div>;
+};
 
 export default Detail;
